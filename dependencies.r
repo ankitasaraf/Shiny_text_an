af12ee474562fec413ae525455cf4a71c9d3b0f9
Ankita Saraf <saraf.ankita90@gmail.com>
2018-06-02 14:20:56 +0530

    Create dependencies.r

---
+++ dependencies.r
@@ -0,0 +1,25 @@
+# setup code block
+if (!require(stringr)){install.packages("stringr")}
+if (!require(udpipe)){install.packages("udpipe")}
+if (!require(textrank)){install.packages("textrank")}
+if (!require(lattice)){install.packages("lattice")}
+if (!require(igraph)){install.packages("igraph")}
+if (!require(ggraph)){install.packages("ggraph")}
+if (!require(wordcloud)){install.packages("wordcloud")}
+if (!require(RColorBrewer)){install.packages("RColorBrewer")}
+if (!require(shiny)){install.packages("shiny")}
+if (!require(shinydashboard)){install.packages("shinydashboard")}
+
+library(stringr)
+library(udpipe)
+library(textrank)
+library(lattice)
+library(igraph)
+library(ggraph)
+library(ggplot2)
+library(wordcloud)
+library(stringr)
+library(tm)
+library(RColorBrewer)
+library(shiny)
+library(shinydashboard)
